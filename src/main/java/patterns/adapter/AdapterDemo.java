88aff3b32afabb9524a9d0d9be2c981ed2830bd8
Nazar Lelyak <nazar.lelyak@gmail.com>
2014-07-14 17:51:38 +0300

    add bridge implementation

---
+++ src/main/java/patterns/adapter/AdapterDemo.java
@@ -0,0 +1,52 @@
+package patterns.adapter;
+
+class OldElectricitySystem {
+    public String matchThinSocket() {
+        return "220V from thin socket";
+    }
+}
+
+interface INewElectricitySystem {
+    String matchWideSocket();
+}
+
+class NewElectricitySystem implements INewElectricitySystem {
+
+    @Override
+    public String matchWideSocket() {
+        return "220V from wide socket";
+    }
+}
+
+class Adapter implements INewElectricitySystem {
+
+    private OldElectricitySystem adaptee;
+
+    Adapter(OldElectricitySystem adaptee) {
+//        System.out.println("adapter created");
+        this.adaptee = adaptee;
+    }
+
+    @Override
+    public String matchWideSocket() {
+        System.out.print("adapter matches for wide socket ");
+        return adaptee.matchThinSocket();
+    }
+}
+
+class ElectricityCustomer {
+    // this laptop only for new system for wide sockets
+    public static void chargeLaptop(INewElectricitySystem electricitySystem) {
+        System.out.println("laptop is charged from " + electricitySystem.matchWideSocket());
+    }
+}
+
+public class AdapterDemo {
+    public static void main(String[] args) {
+        NewElectricitySystem newElectricitySystem = new NewElectricitySystem();
+        ElectricityCustomer.chargeLaptop(newElectricitySystem);
+
+        OldElectricitySystem oldElectricitySystem = new OldElectricitySystem();
+        ElectricityCustomer.chargeLaptop(new Adapter(oldElectricitySystem));
+    }
+}
---
+++ src/main/java/patterns/bridge/BridgeDemo.java
@@ -0,0 +1,103 @@
+package patterns.bridge;
+
+interface IBuildingCompany {
+    void buildFoundation();
+
+    void buildRoof();
+
+    void buildRoom();
+}
+
+interface IWallCreator {
+    void buildWallWithDoor();
+
+    void buildWallWithWindow();
+
+    void buildWall();
+}
+
+class BuildingCompany implements IBuildingCompany {
+
+    private IWallCreator wallCreator;
+
+    @Override
+    public void buildFoundation() {
+        System.out.println("Foundation is built\n");
+    }
+
+    @Override
+    public void buildRoof() {
+        System.out.println("Roof is done");
+    }
+
+    @Override
+    public void buildRoom() {
+        wallCreator.buildWallWithDoor();
+        wallCreator.buildWall();
+        wallCreator.buildWallWithWindow();
+        wallCreator.buildWall();
+        System.out.println("Room is done\n");
+    }
+
+    public IWallCreator getWallCreator() {
+        return wallCreator;
+    }
+    public void setWallCreator(IWallCreator wallCreator) {
+        this.wallCreator = wallCreator;
+    }
+}
+
+class ConcreteWallCreator implements IWallCreator {
+
+    @Override
+    public void buildWallWithDoor() {
+        System.out.println("Concrete wall with door");
+    }
+
+    @Override
+    public void buildWallWithWindow() {
+        System.out.println("Concrete wall with window");
+    }
+
+    @Override
+    public void buildWall() {
+        System.out.println("Concrete wall");
+    }
+}
+
+class BrickWallCreator implements IWallCreator {
+
+    @Override
+    public void buildWallWithDoor() {
+        System.out.println("Brick wall with door");
+    }
+
+    @Override
+    public void buildWallWithWindow() {
+        System.out.println("Brick wall with window");
+    }
+
+    @Override
+    public void buildWall() {
+        System.out.println("Brick wall");
+    }
+}
+
+public class BridgeDemo {
+    public static void main(String[] args) {
+        BuildingCompany buildingCompany = new BuildingCompany();
+
+        ConcreteWallCreator concreteWallCreator = new ConcreteWallCreator();
+        BrickWallCreator brickWallCreator = new BrickWallCreator();
+
+        buildingCompany.buildFoundation();
+        buildingCompany.setWallCreator(concreteWallCreator);
+        buildingCompany.buildRoom();
+
+        // switch to another wall type
+        buildingCompany.setWallCreator(brickWallCreator);
+        buildingCompany.buildRoom();
+        buildingCompany.buildRoom();
+        buildingCompany.buildRoof();
+    }
+}
